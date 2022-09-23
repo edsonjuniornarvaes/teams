ae91902190ca9ae52054ba416b7474a141a9097d
Edson Junior <edsonj@pop-os.localdomain>
2022-09-23 18:43:14 -0300

    feat: implements fonts and loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import { ThemeProvider } from "styled-components";
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
 import theme from "./src/theme";
 
 import Groups from "@screens/Groups";
 
+import { Loading } from "src/components/Loading";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
