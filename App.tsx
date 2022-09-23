728630eeccc2ee5c3cf9b510edbc2942b6508ba2
Edson Junior <edsonj@pop-os.localdomain>
2022-09-23 18:24:30 -0300

    feat: implements theme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components";
+
+import theme from "./src/theme";
+
 import Groups from "@screens/Groups";
 
 export default function App() {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Container } from "@screens/Groups/styles";
 
 export default function Groups() {
-  return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
-  );
+  return <Container></Container>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
