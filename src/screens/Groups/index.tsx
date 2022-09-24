82751b897f7458ac50397c4efd27b993a9055144
Edson Junior <edsonj@pop-os.localdomain>
2022-09-24 12:18:34 -0300

    feat: implements icons and highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@screens/Groups/styles";
 import { Header } from "src/components/header";
+import { Highlight } from "src/components/Highlight";
 
 export default function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 }
