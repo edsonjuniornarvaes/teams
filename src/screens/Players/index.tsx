3ea1be20b1746d787077493f393ac460c8e9eadc
Edson Junior <edsonjunior@pop-os.localdomain>
2022-10-13 19:40:35 -0300

    feat: implements more components

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
+import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { Container } from "./styles";
+import { Input } from "@components/Input";
+import { Container, Form } from "./styles";
 
 export function Players() {
   return (
@@ -10,6 +12,10 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }
